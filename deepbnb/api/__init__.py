1d9d043386f045ed597f810c6945ff7cc3657acc
David Turvey <71280728+Turvey87@users.noreply.github.com>
2022-10-13 13:07:29 -0500

    Small updates to settings

---
+++ deepbnb/api/__init__.py
@@ -0,0 +1,2 @@
+# Example Usage
+# scrapy crawl airbnb -a query="Kansas City, Missouri" -a checkin=2022-10-01 -a checkout=2022-10-10 -a max_price=145 -a min_price=90 -a neighborhoods="Volker" -s WEB_BROWSER="C:\Program Files\Google\Chrome\Application\chrome.exe" -o kansas_city_01.csv
